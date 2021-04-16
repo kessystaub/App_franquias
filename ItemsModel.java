4be3dd82273b81043ade01060275c1bc19a3ac95
kessystaub <51220952+kessystaub@users.noreply.github.com>
2021-04-15 23:42:13 -0300

    Update ItemsModel.java

--- ItemsModel.java
+++ ItemsModel.java
@@ -1,16 +1,58 @@
-package com.example.aplicativoandroid;
+package com.example.myproject;
+
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ItemsModel implements Serializable {
+public class ItemsModel implements Parcelable {
     private String name;
     private String desc;
     private int image;
+    private Restaurante test;
 
-    public ItemsModel(String name, String desc, int image) {
+    public List<Restaurante> restaurantes = new ArrayList();
+
+
+    public ItemsModel(String name, String desc, int image, Restaurante restaurante) {
         this.name = name;
         this.desc = desc;
         this.image = image;
+        //this.restaurantes = restaurantes;
+        this.test=new Restaurante(restaurante.name,restaurante.desc,restaurante.image);
+        this.restaurantes.add(test);
+        this.test=new Restaurante(restaurante.name,restaurante.desc,restaurante.image);
+        this.restaurantes.add(test);
+        this.test=new Restaurante(restaurante.name,restaurante.desc,restaurante.image);
+        this.restaurantes.add(test);
+        this.test=new Restaurante(restaurante.name,restaurante.desc,restaurante.image);
+        this.restaurantes.add(test);
+
+    }
+
+    protected ItemsModel(Parcel in) {
+        name = in.readString();
+        desc = in.readString();
+        image = in.readInt();
+    }
+
+    public static final Creator<ItemsModel> CREATOR = new Creator<ItemsModel>() {
+        @Override
+        public ItemsModel createFromParcel(Parcel in) {
+            return new ItemsModel(in);
+        }
+
+        @Override
+        public ItemsModel[] newArray(int size) {
+            return new ItemsModel[size];
+        }
+    };
+
+
+    public Restaurante getRestaurante(int position){
+        return this.restaurantes.get(position);
     }
 
     public String getName() {
@@ -36,4 +78,18 @@ public class ItemsModel implements Serializable {
     public void setImage(int image) {
         this.image = image;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(desc);
+        dest.writeInt(image);
+    }
 }
+
