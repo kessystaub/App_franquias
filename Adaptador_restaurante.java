8804cde11b3d125766d2ddef38a66b02e816a934
kessystaub <51220952+kessystaub@users.noreply.github.com>
2021-04-16 21:21:05 -0300

    Update and rename Adapter2.java to Adaptador_restaurante.java

--- Adaptador_restaurante.java
+++ Adaptador_restaurante.java
@@ -10,13 +10,13 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class Adapter2 extends BaseAdapter{
+public class Adaptador_restaurante extends BaseAdapter{
 
         Activity context;
         ArrayList<Restaurante> restaurantes;
         private static LayoutInflater inflater = null;
 
-        public Adapter2(Activity context, ArrayList<Restaurante>restaurantes){
+        public Adaptador_restaurante(Activity context, ArrayList<Restaurante>restaurantes){
             this.context = context;
             this.restaurantes = restaurantes;
             inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
@@ -40,14 +40,17 @@ public class Adapter2 extends BaseAdapter{
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View itemview = convertView;
-            itemview = (itemview == null) ? inflater.inflate(R.layout.row_items,null): itemview;
-            TextView textViewName = (TextView) itemview.findViewById(R.id.itemName);
-            TextView textViewDesc = (TextView) itemview.findViewById(R.id.itemDesc);
-            ImageView imageview = itemview.findViewById(R.id.imageView);
+            itemview = (itemview == null) ? inflater.inflate(R.layout.row_items2,null): itemview;
+            TextView textViewName = (TextView) itemview.findViewById(R.id.IDName);
+            TextView textViewAdress = (TextView) itemview.findViewById(R.id.IDAdress);
+            TextView textViewCity = (TextView) itemview.findViewById(R.id.IDCity);
+            ImageView imageview = itemview.findViewById(R.id.IDimage);
             Restaurante selectedRestaurante = restaurantes.get(position);
             textViewName.setText(selectedRestaurante.name);
-            textViewDesc.setText(selectedRestaurante.desc);
+            textViewAdress.setText(selectedRestaurante.adress);
+            textViewCity.setText(selectedRestaurante.city);
+            imageview.setImageResource(selectedRestaurante.image);
+
             return itemview;
         }
-
 }
